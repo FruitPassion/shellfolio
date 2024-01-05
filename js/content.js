21238f9bf6d860a5ac50794e8147265b7f9d2c37
FruitPassion <gamehotdl@gmail.com>
2024-01-05 01:57:24 +0100

    initial commit

---
+++ js/content.js
@@ -0,0 +1,84 @@
+function random_ip(){
+    return (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))
+}
+
+function random_date(){
+    let currentDate = new Date();
+
+    let yesterday = new Date(currentDate);
+    yesterday.setDate(currentDate.getDate() - 1);
+
+    let dayName = yesterday.toLocaleDateString('en-US', { weekday: 'short' });
+    let monthName = yesterday.toLocaleDateString('en-US', { month: 'short' });
+
+    let dayOfMonth = yesterday.getDate();
+
+    let formattedDate = `${dayName} ${monthName} ${dayOfMonth}`;
+
+    return formattedDate;
+}
+
+function random_time() {
+    let hours = Math.floor(Math.random() * 24);
+    let minutes = Math.floor(Math.random() * 60);
+    let seconds = Math.floor(Math.random() * 60);
+    
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    let formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    return formattedTime;
+}
+
+let spacedebut = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+
+let preheader = [
+    '<p>Bienvue sur shellfolio.fruitpassion.fr</p>',
+    '<p>Derniere connexion: '+random_date()+' '+random_time()+' 2024 depuis '+random_ip()+'</p>'
+]
+
+
+let banniere = [
+    '<br>',
+    '  sSSs   .S_sSSs     .S       S.    .S  sdSS_SSSSSSbs   .S_sSSs     .S_SSSs      sSSs    sSSs   .S    sSSs_sSSs     .S_sSSs            sSSs   .S_sSSs    ',
+    '  d%%SP  .SS~YS%%b   .SS       SS.  .SS  YSSS~S%SSSSSP  .SS~YS%%b   .SS~SSSSS    d%%SP   d%%SP  .SS   d%%SP~YS%%b   .SS~YS%%b          d%%SP  .SS~YS%%b   ',
+    ' d%S     S%S    S%b  S%S       S%S  S%S       S%S       S%S    S%b  S%S   SSSS  d%S     d%S     S%S  d%S       S%b  S%S    S%b        d%S     S%S   `S%b  ',
+    ' S%S     S%S    S%S  S%S       S%S  S%S       S%S       S%S    S%S  S%S    S%S  S%|     S%|     S%S  S%S       S%S  S%S    S%S        S%S     S%S    S%S  ',
+    ' S&S     S%S    d*S  S&S       S&S  S&S       S&S       S%S    d*S  S%S SSSS%S  S&S     S&S     S&S  S&S       S&S  S%S    S&S        S&S     S%S    d*S  ',
+    ' S&S_Ss  S&S   .S*S  S&S       S&S  S&S       S&S       S&S   .S*S  S&S  SSS%S  Y&Ss    Y&Ss    S&S  S&S       S&S  S&S    S&S        S&S_Ss  S&S   .S*S  ',
+    ' S&S~SP  S&S_sdSSS   S&S       S&S  S&S       S&S       S&S_sdSSS   S&S    S&S  `S&&S   `S&&S   S&S  S&S       S&S  S&S    S&S        S&S~SP  S&S_sdSSS   ',
+    ' S&S     S&S~YSY%b   S&S       S&S  S&S       S&S       S&S~YSSY    S&S    S&S    `S*S    `S*S  S&S  S&S       S&S  S&S    S&S        S&S     S&S~YSY%b   ',
+    ' S*b     S*S   `S%b  S*b       d*S  S*S       S*S       S*S         S*S    S&S     l*S     l*S  S*S  S*b       d*S  S*S    S*S        S*b     S*S   `S%b  ',
+    ' S*S     S*S    S%S  S*S.     .S*S  S*S       S*S       S*S         S*S    S*S    .S*P    .S*P  S*S  S*S.     .S*S  S*S    S*S        S*S     S*S    S%S  ',
+    ' S*S     S*S    S&S   SSSbs_sdSSS   S*S       S*S       S*S         S*S    S*S  sSS*S   sSS*S   S*S   SSSbs_sdSSS   S*S    S*S        S*S     S*S    S&S  ',
+    ' S*S     S*S    SSS    YSSP~YSSY    S*S       S*S       S*S         SSS    S*S  YSS     YSS     S*S    YSSP~YSSY    S*S    SSS   SS   S*S     S*S    SSS  ',
+    ' SP      SP                         SP        SP        SP                 SP                   SP                  SP          S%%S  SP      SP          ',
+    ' Y       Y                          Y         Y         Y                  Y                    Y                   Y            SS   Y       Y           ',
+    '<br>'
+]
+
+let postheader = [
+    '<p>Bienvenue sur mon terminal web.</p>',
+    "<p>Pour la liste des commandes disponibles, entrez <span class='hintcolor'>'help'</span>.</p>"
+]
+
+let line_clone = ['<div style="display: flex;"><p class="visiteurcolor">visiteur@shellfolio.fruitpassion.fr: ~$&nbsp;</p><p>','</p>']
+
+let whois = [
+    "<p>"+spacedebut+"Salut, je m'appel Raphael.</p>",
+    "<p>"+spacedebut+"Je suis un étudiant en informatique orienté en sécurité et web.</p>",
+    "<p>"+spacedebut+"A completer...</p>"
+]
+
+let help = [
+    '<p>'+spacedebut+'<span class="hintcolor">whois</span> : Qui suis-je ?</p>',
+    '<p>'+spacedebut+'<span class="hintcolor">ls</span> : Lister mes projets</p>',
+    '<p>'+spacedebut+'<span class="hintcolor">pwd</span> : Lister mes sites</p>',
+    '<p>'+spacedebut+'<span class="hintcolor">clear</span> : Nettoyer le terminal.</p>',
+    '<p>'+spacedebut+'<span class="hintcolor">echo</span> : Afficher quelque chose.</p>',
+    '<p>'+spacedebut+'<span class="hintcolor">banniere</span> : Afficher la banniere.</p>',
+    '<p>'+spacedebut+'<span class="hintcolor">help</span> : Afficher cette commande.</p>'
+]
---
+++ js/main.js
@@ -0,0 +1,101 @@
+const delay = millis => new Promise((resolve, reject) => {
+    setTimeout(_ => resolve(), millis)
+});
+
+let useri = document.getElementById("user-input")
+let texter = document.getElementById('texter')
+let before = document.getElementById("before");
+
+bootup();
+
+useri.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        gerer_commande(texter.value)
+    }
+});
+
+function gerer_commande(commande){
+    commande = commande.replace(/[\r\n]+/g, "");
+    let temp = '';
+    switch (commande.split(" ")[0]) {
+        case 'clear':
+            before.innerHTML = "";
+            break;
+        case '':
+            copierligne(commande);
+            break;
+        case 'banniere':
+            copierligne(commande);
+            commande_banniere()
+            break;
+        case 'whois':
+            copierligne(commande);
+            iterateList(whois, replace=false, header=false);
+            break;
+        case 'ls':
+            copierligne(commande);
+            before.innerHTML += "<p>"+spacedebut+"A faire ...</p>";
+            break;
+        case 'pwd':
+            copierligne(commande);
+            before.innerHTML += "<p>"+spacedebut+"A faire ...</p>";
+            break;
+        case 'help':
+            copierligne(commande);
+            iterateList(help, replace=false, header=false);
+            break;
+        case 'echo':
+            copierligne(commande);
+            commande = commande.split(" ")
+            for (var i = 1; i < commande.length; i++) {
+                temp += commande[i] + ' '
+            }
+            before.innerHTML += temp;
+            break;
+        default:
+            copierligne(commande);
+            before.innerHTML += "<p class='errorcolor'>"+spacedebut+"shellfolio: " + commande.split(" ")[0] + ": commande non trouvée</p>";
+    }
+    texter.value = "";
+}
+
+async function bootup(){
+    commande_banniere();
+    await delay(1210);
+    useri.style.display = "flex";
+    useri.click();
+}
+
+async function commande_banniere(){
+    iterateList(preheader, replace=true, header=false);
+    await delay(120);
+    iterateList(banniere, replace=true, header=true);
+    await delay(960);
+    iterateList(postheader, replace=false, header=false);
+    await delay(120);
+}
+
+async function iterateList(liste, replace=true, header=false){
+    for (let element of liste) { 
+        if (replace) {
+            element = replaceAll(element, " ", "&nbsp;")
+        }
+        if (header){
+            element = '<p class="headercolor">'+ element + '</p>'
+        }
+        before.innerHTML += element;
+        await delay(60);
+    } 
+}
+
+function replaceAll(string, search, replace) {
+    return string.split(search).join(replace);
+}
+
+function ecrire(text){
+    document.getElementById('typer').innerText = text.replace(/[\r\n]+/g, "");
+}
+
+function copierligne(commande){
+    before.innerHTML += line_clone[0] + commande + line_clone[1];
+}
